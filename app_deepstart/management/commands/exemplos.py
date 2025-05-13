b2e741f8ef6b53c2cef396418fff75ad963c669a
lbulegon <lbulegon@gmail.com>
2025-05-13 01:50:42 -0300

    add

--- app_deepstart/management/commands/exemplos.py
+++ app_deepstart/management/commands/exemplos.py
@@ -1,14 +1,11 @@
 
 import spacy       
 
-
 nlp = spacy.load("pt_core_news_sm")        
 
 doc = nlp("Ulisses foi ao bar Overflow no centro da cidade de São Paulo.")   
 for token in doc:
     print(f"TOKEN: {token.text}\tLEMMA: {token.lemma_}\tPOS: {token.pos_}\tTAG: {token.tag_}\tDEP: {token.dep_}")
-
-
 
 doc = nlp("O presidente Lula visitou Brasília em janeiro de 2023.")
 
